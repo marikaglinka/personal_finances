9a16f9842b2b6ecd1775f1254e6dac94ac8975ce
Marika <marika.glinka@gmail.com>
2024-03-01 22:43:46 +0100

    base for application

---
+++ src/main/java/com/personalfinances/personalFinances/PersonalFinancesApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinances.personalFinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalfinances/personalFinances/PersonalFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalfinances.personalFinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
